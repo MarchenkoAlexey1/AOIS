11e65dc64ebadc854c42d879eebc26dfa5ba5c59
MarchenkoAlexey1 <marchenko200507@gmail.com>
2024-04-13 20:55:44 +0300

    lab2

---
+++ lab2/TruthTableTests.cpp
@@ -0,0 +1,37 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\lab2\TruthTable.h"
+#include "..\lab2\TruthTable.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TruthTableTests
+{
+	TEST_CLASS(TruthTableTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			TruthTable A("a~!a");
+			Assert::IsTrue(A.GetIndexForm() == 0);
+			Assert::IsTrue(A.GetResult(0) == 0);
+			Assert::IsTrue(A.GetResult(1) == 0);
+		}
+
+		TEST_METHOD(TestMethod2)
+		{
+			TruthTable A("(a>b)&a");
+			A.ConstructionOfSDNFandSKNF();
+			Assert::IsTrue(A.GetSDNF() == "(a&!b)|(a&b)");
+			Assert::IsTrue(A.GetSKNF() == "(a|b)&(a|!b)");
+		}
+
+		TEST_METHOD(TestMethod3)
+		{
+			TruthTable A("!a&(b|c)");
+			A.ConstructionOfSDNFandSKNF();
+			Assert::IsTrue(A.GetIndexForm() == 14);
+		}
+	};
+}
